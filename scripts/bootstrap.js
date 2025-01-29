4759a6df8bfa88a46d2ed6d52a28df482c0b8a71
Christopher W. Blackwell <cwblackwell@gmail.com>
2025-01-29 08:14:11 -0500

    v.1.0.0

---
+++ scripts/bootstrap.js
@@ -0,0 +1,63 @@
+
+/* We want to stage all the loading, ending with "announce ready" */
+
+// Load the index…
+$.get( indexFileName, function( data ) {
+  loadIndex(data);
+
+  // …then load the lexicon…
+  $.get( lexFileName, function( data ) {
+    loadLexicon(data);
+
+    // …then initiate betacode typing…
+    $.getScript("scripts/betatyper.js", function () {
+
+      // …then activate other interactions…
+      $.getScript("scripts/interactions.js", function () {
+
+        // …then configure to start.
+        initialAlphabetListState();
+        announceReady(); // This will be the innermost call.
+
+      });
+    });
+  });
+
+
+});
+
+
+function announceReady() {
+  messageInfo("All data loaded.");
+}
+
+
+function loadIndex(data) {
+
+    var lineArray = data.split(/\r?\n/).join("\n").split("\n");
+
+    indexData = lineArray.map( (l) => {
+        let lem = l.split("#")[0];
+        let llem = l.split("#")[1];
+        let u = l.split("#")[2];
+        let gw = l.split("#")[3];
+        return { lemma: lem, lookuplemma: llem, urn: u, greekwords: gw};
+    });
+}
+
+function loadLexicon(data) {
+
+     var lineArray = data.split(/\r?\n/).join("\n").split("\n");
+
+     lexData = lineArray.map( (l) => {
+        let s = parseInt(l.split("#")[0]);
+        let u = l.split("#")[1];
+        let k = l.split("#")[2];
+        let e = l.split("#")[3];
+        return { seq: s, urn: u, key: k, entry: e};
+    });
+
+}
+
+
+
--- scripts/markdownCite.js
+++
@@ -1,6 +0,0 @@
-function initiateMarked(markdownString, elementId) {
-	setTimeout(function(){
-		var parsedMarkdown = marked(markdownString).replace("<p>","").replace("</p>","")
-		document.getElementById(elementId).innerHTML = parsedMarkdown;
-	},1000);
-}
